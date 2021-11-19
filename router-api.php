1cedf346d8b72e2e13a5c3a107db49969ba30097
Ulises Zapata <aaronabella1382@gmail.com>
2021-11-19 17:31:38 -0300

    agregue unas rutas god

--- router-api.php
+++ router-api.php
@@ -5,9 +5,16 @@ require_once 'Controller/ApiDropsController.php';
 
 $router = new Router();
 
-$router-> addroute();
-$router-> addroute();
-$router-> addroute();
+$router-> addroute('zapatillas','GET','ApiDropsController','getSneakers');
+$router-> addroute('zapatillas/:ID','GET','ApiDropsController','getSneaker');
+$router-> addroute('zapatillas/:ID','DELETE''ApiDropsController','deleteSneaker');
+$router-> addroute('zapatillas/','POST','ApiDropsController','insertSneaker');
+$router-> addroute('zapatillas/','PUT','ApiDropsController','editSneaker');
+$router-> addroute('marcas','GET','ApiDropsController','getBrands');
+$router-> addroute('marcas/:ID','GET','ApiDropsController','getBrand');
+$router-> addroute('marcas/:ID','DELETE','ApiDropsController','deleteBrand');
+$router-> addroute('marcas/:ID','POST','ApiDropsController','insertBrand');
+$router-> addroute('marcas/:ID','PUT','ApiDropsController','editBrand');
 
 $resource = $_GET['resource'];
 $method = $_SERVER['REQUEST_METHOD'];
