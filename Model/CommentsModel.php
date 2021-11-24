ccc6447879fe9b1979ce4370b2e81bba86eee0d1
Ulises Zapata <aaronabella1382@gmail.com>
2021-11-24 15:19:18 -0300

    ApiComments

---
+++ Model/CommentsModel.php
@@ -0,0 +1,34 @@
+<?php
+
+class CommentsModel{
+    private $db;
+
+    function __construct(){
+        $this->db = new PDO('mysql:host=localhost;'.'dbname=db_gordodrops;charset=utf8', 'root', '');
+    }
+
+    function getCommentsSneakers($id){
+        $query = $this->db->prepare( "SELECT c.*, u.username AS usuario FROM comentarios c LEFT JOIN usuarios u ON c.id_user = u.id_user WHERE id_zapatilla=?");
+        $query->execute(array($id));
+        $comentarios = $query->fetchAll(PDO::FETCH_OBJ);
+        return $comentarios;
+    }
+    
+    function delCommentById($id){
+        $query = $this->db->prepare( "DELETE from comentarios WHERE id_comentario=?");
+        $query->execute(array($id));
+
+    }
+
+    function addComment(){
+        $query = $this->db->prepare("INSERT INTO comentarios(id_zapatilla,id_user, descripcion, puntuacion) VALUES(?, ?, ?, ?)");
+        $query->execute(array($id_zapatilla,$id_user,$descripcion,$puntuacion));
+    }
+
+    function getComment($id){
+        $query = $this->db->prepare( "SELECT from comentarios WHERE id_comentario=?");
+        $query->execute(array($id));
+        $comment= $query->fetch(PDO::FETCH_OBJ);
+        return $comment;
+    }
+}
